3b68ceafed694d0a39a7e1097de95f923630685b
+ <+>
2023-03-20 23:32:51 +0300

    Assignment Done

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,13 +7,21 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
-    render json: toy, status: :created
+    toy = Toy.new(toy_params)
+    if toy.save
+      render json: toy, status: :created
+    else
+      render json: { errors: toy.errors.full_messages }, status: :unprocessable_entity
+    end
   end
+  
+ 
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json: toy
+
   end
 
   def destroy
@@ -27,5 +35,7 @@ class ToysController < ApplicationController
   def toy_params
     params.permit(:name, :image, :likes)
   end
-
+  
+  
+  
 end
